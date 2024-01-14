71d53e0f539b1fa80eebc50c406e8b7ee5980330
Matthew Knorr <matt@knorrs.net>
2024-01-14 13:03:39 -0700

    Bug Fixes Gotta Fix Before Monday

--- obj/Debug/net6.0/ContosoPizza.AssemblyInfo.cs
+++ obj/Debug/net6.0/ContosoPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de84c4868eee7e977da9d7d70439cba97416b830")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd4174a864c8a20d74bf8fb7aa353d0423103485")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
